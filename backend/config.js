ae66638218f061c885a9860673062d9bc03eb264
imana <imanabdella9@gmail.com>
2024-08-31 20:04:17 +1100

    added images

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = 5554;
 
 export const  mongourl = 'mongodb+srv://Iman:Imanovich@imancluster.glndsrs.mongodb.net/Books_collection?retryWrites=true&w=majority&appName=Imancluster'
